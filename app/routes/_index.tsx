e6304df48ef2d6edffb823b1969ca5cd7d321f24
8an3 <skylerzanth@gmail.com>
2024-06-26 12:52:30 -0400

    asd

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,16 @@
+"use client"
+
+
 import type { MetaFunction } from "@remix-run/node";
+import * as React from "react"
+
+
+import { siteConfig } from "~/config/site"
+import { cn } from "~/lib/utils"
+import { Icons } from "~/components/icons"
+import { Badge } from "~/registry/new-york/ui/badge"
+import { Link } from "@remix-run/react";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,31 +20,82 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const pathname = usePathname()
+
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix (SPA Mode)</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/guides/spa-mode"
-            rel="noreferrer"
+      <div className="mr-4 hidden md:flex">
+        <Link to="/" className="mr-6 flex items-center space-x-2">
+          <Icons.logo className="h-6 w-6" />
+          <span className="hidden font-bold sm:inline-block">
+            Michinski Vs. Zanth
+          </span>
+        </Link>
+        <nav className="flex items-center gap-4 text-sm lg:gap-6">
+          <Link
+            to="/application"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname === "/application" ? "text-foreground" : "text-foreground/60"
+            )}
           >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
+            Application
+          </Link>
+          <Link
+            to="/answer"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname?.startsWith("/answer")
+                ? "text-foreground"
+                : "text-foreground/60"
+            )}
           >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            Answer
+          </Link>
+          <Link
+            to="/reply"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname?.startsWith("/reply")
+                ? "text-foreground"
+                : "text-foreground/60"
+            )}
+          >
+            Reply
+          </Link>
+          <Link
+            to="/noticeOfMotion"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname?.startsWith("/noticeOfMotion")
+                ? "text-foreground"
+                : "text-foreground/60"
+            )}
+          >
+            Notice of Motion
+          </Link>
+          <Link
+            to="/exhibits"
+            className={cn(
+              "transition-colors hover:text-foreground/80",
+              pathname?.startsWith("/exhibitsp")
+                ? "text-foreground"
+                : "text-foreground/60"
+            )}
+          >
+            Exhibits
+          </Link>
+          <Link
+            to={siteConfig.links.github}
+            className={cn(
+              "hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block"
+            )}
+          >
+            GitHub
+          </Link>
+        </nav>
+      </div>
+
     </div>
   );
 }
