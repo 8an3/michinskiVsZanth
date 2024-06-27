5dd3509e4b18ad4c3e0dcc3dcf747ce55ddd06b5
8an3 <skylerzanth@gmail.com>
2024-06-26 20:59:01 -0400

    kljh

--- app/routes/exhibits.tsx
+++ app/routes/exhibits.tsx
@@ -1,9 +1,20 @@
+
+
 import {
   ClientActionFunctionArgs,
   useFetcher,
   useLoaderData,
 } from "@remix-run/react";
 import { useEffect, useRef } from "react";
+import first from '~/courtcase/1.jpg'
+import second from '~/courtcase/2.jpg'
+import third from '~/courtcase/3.jpg'
+import forth from '~/courtcase/4.jpg'
+import fith from '~/courtcase/5.jpg'
+import sixth from '~/courtcase/6.jpg'
+import seventh from '~/courtcase/7.jpg'
+import eight from '~/courtcase/8.jpg'
+
 
 export async function clientLoader() {
   return { ok: true };
@@ -15,8 +26,46 @@ export async function clientAction({ request }: ClientActionFunctionArgs) {
 export default function NewFile() {
 
   return (
-    <div>
-      <p>test</p>
+    <div className="relative rounded-xl overflow-auto">
+      <div className="flex ml-[50%] items-end justify-start pt-10 mb-6">
+        <div className="ml-2 rounded font-mono text-[0.625rem] leading-6 px-1.5 ring-1 ring-inset bg-indigo-50 text-indigo-600 ring-indigo-600 dark:bg-indigo-500 dark:ring-0 dark:text-white dark:highlight-white/10">snap point</div>
+        <div className="absolute top-0 bottom-0 left-1/2 border-l border-indigo-500"></div>
+      </div>
+      <div className="relative w-full flex gap-6 snap-x overflow-x-auto pb-14">
+        <div className="snap-center shrink-0">
+          <div className="shrink-0 w-4 sm:w-48"></div>
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <div className='grid grid-cols-1'>
+            <img className="shrink-0 w-[600px] h-[750px] object-cover rounded-lg shadow-xl bg-white" src={first} />
+          </div>
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <div className='grid grid-cols-1'>
+            <img className="shrink-0 w-[700px] h-[300px] object-cover rounded-lg shadow-xl bg-white" src={second} />
+            <img className="shrink-0 w-[700px] h-[300px] mt-5 object-cover rounded-lg shadow-xl bg-white" src={third} />
+          </div>
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <div className='grid grid-cols-1'>
+            <img className="shrink-0 w-[700px] h-[300px] object-cover rounded-lg shadow-xl bg-white" src={forth} />
+            <img className="shrink-0 w-[700px] h-[300px] mt-5 object-cover rounded-lg shadow-xl bg-white" src={fith} />
+          </div>
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <img className="shrink-0 w-[600px] h-[750px] object-cover rounded-lg shadow-xl bg-white" src={sixth} />
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <img className="shrink-0 w-[600px] h-[750px] object-cover rounded-lg shadow-xl bg-white" src={seventh} />
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <img className="shrink-0 w-[600px] h-[750px] object-cover rounded-lg shadow-xl bg-white" src={eight} />
+        </div>
+        <div className="snap-center shrink-0">
+          <div className="shrink-0 w-4 sm:w-48"></div>
+        </div>
+      </div>
     </div>
+
   )
 }
--- app/routes/todo.tsx
+++ app/routes/todo.tsx
@@ -1,9 +1,177 @@
 
-export default function NewFile() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+
+export default function Todo() {
 
   return (
     <div>
-      <p>test</p>
+      <Card className='max-h-[800px] h-[800px] overflow-y-scroll'>
+        <CardContent className="space-y-2">
+          <div className="font-semibold mt-5">Need to charge the cunt cow more money</div>
+          <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+          <ul className="grid gap-3">
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                rule 18 of the family law rules, no reasonable offer given by applicant.
+              </span>
+              <span>
+                <p className='text-right'>
+                  pays all fees associated with the case<br />
+                  175<br />
+                  200<br />
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                I was supposed to get money on refinance, the balance of what natashia did need after the 40k
+              </span>
+              <span>
+                <p className='text-right'>
+                  have natashia provide evidence anything that you did not get, you get now <br />
+                  $15,000
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                I paid half her debt less than a year ago
+              </span>
+              <span>
+                <p className='text-right'>
+                  natashia nows needs to step up<br />
+                  rbc checking - 3500<br />
+                  rbc loc - 8500<br />
+                  car -  15,000<br />
+                  windows - 20,000<br />
+                  scotia loc - 42,000<br />
+                  scotia visa - 3200<br />
+                  DO A CREDIT CHECK TO SEE WHAT ELSE THERE IS
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                restitution of wages
+              </span>
+              <span>
+                <p className='text-right'>
+                  based of 94k a year which comes up to 1807 a week at this time she owes 19,884
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                purposly making my accounts go into arrears
+              </span>
+              <span>
+                <p className='text-right'>
+                  pays for dmg and brings the acounts up to date
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                items taken from home
+              </span>
+              <span>
+                <p className='text-right'>
+                  5500
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                cost to repair damaged credit
+              </span>
+              <span>
+                <p className='text-right'>
+                  $
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                sales of house
+              </span>
+              <span>
+                <p className='text-right'>
+                  50%
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+            <li className="flex items-center justify-between">
+              <span className="text-[#8a8a93]">
+                i should not have to pay rent, natashia and her counsel promised me that i would get a fair offer the first week and promised me it would be fair so much so they told me to start packing, since then i have sent natashia 6 offers,
+                natashia sent 1, the second her lawywer had to talk her into it because it was so unfair. at any time she could had stop trying to lie to get the entire house and end this fairly, the length of time of this separtion falls entirelyt on natashia and her counsel. i should not have to pay natashia anything in regards about any matter pertaining to this case since shes the one lying about her false claims, shes the one dragging it out this long not me all these are her actions
+              </span>
+              <span>
+                <p className='text-right'>
+                </p>
+              </span>
+            </li>
+            <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+
+          </ul>
+
+          <div className="font-semibold mt-5">To-Do</div>
+          <hr className="my-4 text-muted-foreground w-[95%] mx-auto" />
+          <p>
+            - next court data: july 2nd @ 8:45 am
+          </p>
+          <p>
+            - 29 june next ei report
+          </p>
+          <p>
+            DONE -- MEET WITH NOTARY THEN FILE -- file a motion on Monday to get Natashia to pay the arrears - need to inquire about car finance loan natashia still hasnt given the information maybe theres a special motion for that
+          </p>
+          <p>
+            DONE -- request evidence -- all correspondance between natashia and the refinance company
+          </p>
+          <p>
+            - request evidence --   follow up with jean as he will also have stuff for u
+          </p>
+          <p>
+            DONE -- request evidence --   statements of all accounts during our relationship that natashia had
+          </p>
+          <p>
+            DONE -- NO ANSWER -- call those pricks at scotia branch since they havent gotten back to you
+            file paperwork<br />
+            called, made a complaint ecmf(z)00240624-2267<br />
+            excalatedconcerns@scotiabank.com<br />
+            1-877-700-0043<br />
+            call when u can
+
+          </p>
+          <p>as of now without the debt and without the proceeds from the sale of the home, we are at<br /> 39,959.615</p>
+          <p>half the debts are another <br />36,100</p>
+          <p>we then have the matter of the home should should be able 100k split into 2</p>
+          <p>because the abuse effected my professional life she should have to pay the difference, in pay lost in comparison to pay earned. i would have gotten paid this if these abuse events hadnt taken place i found out prior to feb 23 2022, i'm trying to find the orginal doctors appoints that can be proven right now i just have a requisitoin from feb 23 so i can this date at the very least. </p>
+        </CardContent>
+      </Card>
     </div>
   )
 }
